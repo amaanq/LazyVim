0ffffc15a51541f70691f56c088e0d89a935e198
Amaan Qureshi <git@amaanq.com>
2026-02-21 05:24:03 -0500

    extras: add noice extra, rounded borders for blink.cmp
    Move noice customization (views, presets, routes, lsp overrides) into a
    dedicated extras/ui/noice.lua loaded via xtras.lua, since lazyvim.plugins
    sub-module specs are not applied by lazy.nvim at runtime. Add rounded
    borders to blink.cmp completion menu and documentation window in the
    blink extra.

---
+++ lua/lazyvim/plugins/extras/ui/noice.lua
@@ -0,0 +1,129 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+      },
+      commands = {
+        all = {
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      },
+      views = {
+        mini = {
+          win_options = {
+            winblend = 0,
+            winhighlight = { Normal = "Pmenu", FloatBorder = "Pmenu" },
+          },
+        },
+        cmdline_popup = {
+          position = { row = 5, col = "50%" },
+          size = { width = 60, height = "auto" },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = { row = 8, col = "50%" },
+          size = { width = 60, height = 10 },
+          border = { style = "rounded", padding = { 0, 1 } },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        cmdline_output_to_split = false,
+        lsp_doc_border = true,
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sn", "", desc = "+noice"},
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+      { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+    },
+    config = function(_, opts)
+      -- HACK: noice shows messages from before it was enabled,
+      -- but this is not ideal when Lazy is installing plugins,
+      -- so clear the messages in this case.
+      if vim.o.filetype == "lazy" then
+        vim.cmd([[messages clear]])
+      end
+
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          ["not"] = {
+            event = "lsp",
+            kind = "progress",
+          },
+          event = "msg_show",
+          find = "%d+L, %d+B",
+          cond = function()
+            return not focused and false
+          end,
+        },
+        view = "mini",
+        opts = { stop = false, replace = true },
+      })
+
+      require("noice").setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+    end,
+  },
+}
